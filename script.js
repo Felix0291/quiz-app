5d627528c4d4b372a400526bc091d59d282a7581
Felix0291 <felix.tapper@gmail.com>
2024-10-16 17:10:32 +0200

    added css media query

--- script.js
+++ script.js
@@ -1,71 +1,66 @@
-// import {musicQuestion, bookQuestions} from "../Uppgift-1-Quizapplikation/question.js"
-
-
-
-    const musicQuestion = [
+const musicQuestions = [
     
-        {
-            question: "Which part of New York is the Hip Hop group Wu-Tang Clan from?",
-            options: ["Staten Island", "Brooklyn", "Queens", "Harlem" ],
-            correctAnswer: "Staten Island"
-        },
+    {
+        question: "Which part of New York is the Hip Hop group Wu-Tang Clan from?",
+        options: ["Staten Island", "Brooklyn", "Queens", "Harlem" ],
+        correctAnswer: "Staten Island"
+    },
+    
+    {
+        question: "Who wrote the song Bad? (Relased 1987)",
+        options: ["Elvis", "Adele", "Billy Joel", "Michael Jackson"],
+        correctAnswer: "Michael Jackson"
         
-        {
-            question: "Who wrote the song Bad? (Relased 1987)",
-            options: ["Elvis", "Adele", "Billy Joel", "Michael Jackson"],
-            correctAnswer: "Michael Jackson"
-            
-        },
-        
-        {
-            question: "Who is a member of The Beatles?",
-            options: ["George Michael", "Eminem", "Mark Knofpler", "Ringo Starr" ],
-            correctAnswer: "Ringo Starr"
-        },
-        
-        {
-            question: "What year was Blur's album Parklife released?",
-            options: ["1998", "2001", "1972", "1994"],
-            correctAnswer: "1994"
-        },
-        
-        {
-            question: "How many studio albums have Taylor Swift released?",
-            options: ["10", "18", "5", "11" ],
-            correctAnswer: "11"
-        },
-        
-    ]
+    },
+    
+    {
+        question: "Who is a member of The Beatles?",
+        options: ["George Michael", "Eminem", "Mark Knofpler", "Ringo Starr" ],
+        correctAnswer: "Ringo Starr"
+    },
+    
+    {
+        question: "What year was Blur's album Parklife released?",
+        options: ["1998", "2001", "1972", "1994"],
+        correctAnswer: "1994"
+    },
+    
+    {
+        question: "How many studio albums have Taylor Swift released?",
+        options: ["10", "18", "5", "11" ],
+        correctAnswer: "11"
+    },
+    
+]
 
-    const bookQuestions = [
-        {
-            question: "Who wrote the Harry Potter Books?",
-            options: ["Alfred Nobel", "J.K. Rowling", "George Orwell", "Franz Kafka"],
-            correctAnswer: "J.K. Rowling"
-        },
-        {
-            question: "When was the first Nobel Prize awarded?",
-            options: ["1892", "1924", "2001", "1901"],
-            correctAnswer: "1901"
-        },
-        {
-            question: "Who wrote the famous quote: To be, or not to be?",
-            options: ["Erik Carr", "William Shakespear", "Charles Dickens", "T.S. Eliot"],
-            correctAnswer: "J.K. Rowling"
-        },
-        {
-            question: "How many books/novels has Stephen King written?",
-            options: ["29", "15", "98", "65"],
-            correctAnswer: "65"
-        },
-        {
-            question: "Who turned the Lord Of The Rings books in to movies?",
-            options: ["Cristopher Nolan", "Peter Jackson", "Quention Tarantiono", "Martin Scorsese" ],
-            correctAnswer: "Peter Jackson"
-        },
-        
-    ]
-
+const bookQuestions = [
+    {
+        question: "Who wrote the Harry Potter Books?",
+        options: ["Alfred Nobel", "J.K. Rowling", "George Orwell", "Franz Kafka"],
+        correctAnswer: "J.K. Rowling"
+    },
+    {
+        question: "When was the first Nobel Prize awarded?",
+        options: ["1892", "1924", "2001", "1901"],
+        correctAnswer: "1901"
+    },
+    {
+        question: "Who wrote the famous quote: To be, or not to be?",
+        options: ["Erik Carr", "William Shakespear", "Charles Dickens", "T.S. Eliot"],
+        correctAnswer: "J.K. Rowling"
+    },
+    {
+        question: "How many books/novels has Stephen King written?",
+        options: ["29", "15", "98", "65"],
+        correctAnswer: "65"
+    },
+    {
+        question: "Who turned the Lord Of The Rings books in to movies?",
+        options: ["Cristopher Nolan", "Peter Jackson", "Quention Tarantiono", "Martin Scorsese" ],
+        correctAnswer: "Peter Jackson"
+    },
+    
+]
 
 const mainContainer = document.getElementById("container")
 const subjectBtnMusic = document.getElementById("subject-btn-1")
@@ -84,7 +79,7 @@ let totalScore = 0
 let startTime
 
 
-subjectBtnMusic.addEventListener("click", () => startQuiz(musicQuestion))
+subjectBtnMusic.addEventListener("click", () => startQuiz(musicQuestions))
 subjectBtnBooks.addEventListener("click", () => startQuiz(bookQuestions))
 
 nextBtn.addEventListener("click", () => next())
